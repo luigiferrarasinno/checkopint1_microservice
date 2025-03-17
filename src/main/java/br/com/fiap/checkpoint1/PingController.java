024da45161ffafe1122d927e575270d907b07c5b
Laboratório FIAP <labsfiap@fiap.com.br>
2025-03-17 20:38:34 -0300

    teste

---
+++ src/main/java/br/com/fiap/checkpoint1/PingController.java
@@ -0,0 +1,13 @@
+package br.com.fiap.checkpoint1;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class PingController {
+
+    @GetMapping("/ping")
+    public String ping() {
+        return "pong";
+    }
+}
